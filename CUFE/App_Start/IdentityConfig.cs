005a1b1bfa1ec5c8f0c850e44e4bfbbcfba900df
bajwaimran <bajwa.imran@icloud.com>
2019-01-25 09:52:23 +0100

    First commit 25 Januar 2019

--- CUFE/App_Start/IdentityConfig.cs
+++ CUFE/App_Start/IdentityConfig.cs
@@ -11,6 +11,8 @@ using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
 using CUFE.Models;
+using DX.Data.Xpo.Identity;
+using DX.Data.Xpo;
 
 namespace CUFE
 {
@@ -42,7 +44,9 @@ namespace CUFE
 
         public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context) 
         {
-            var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context.Get<ApplicationDbContext>()));
+            //var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context.Get<ApplicationDbContext>()));
+            var manager = new ApplicationUserManager(
+                new XPUserStore<ApplicationUser, XpoApplicationUser>(context.Get<XpoDatabase>()));
             // Configure validation logic for usernames
             manager.UserValidator = new UserValidator<ApplicationUser>(manager)
             {
--- CUFE/Models/IdentityModels.cs
+++ CUFE/Models/IdentityModels.cs
@@ -1,14 +1,42 @@
 ﻿using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using DevExpress.Xpo;
+using DX.Data.Xpo.Identity;
+using DX.Data.Xpo.Identity.Persistent;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace CUFE.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
-    public class ApplicationUser : IdentityUser
+    public class ApplicationUser : XPIdentityUser<string, XpoApplicationUser>
     {
+        public ApplicationUser(XpoApplicationUser source) :
+        base(source)
+        {
+        }
+
+        public ApplicationUser(XpoApplicationUser source, int loadingFlags) :
+            base(source, loadingFlags)
+        {
+        }
+
+        public ApplicationUser() :
+            base()
+        {
+        }
+        public override void Assign(object source, int loadingFlags)
+        {
+            base.Assign(source, loadingFlags);
+            //XpoApplicationUser src = source as XpoApplicationUser;
+            //if (src != null)
+            //{
+            //	// additional properties here
+            //	this.PropertyA = src.PropertyA;
+            //	// etc.				
+            //}
+        }
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -18,16 +46,88 @@ namespace CUFE.Models
         }
     }
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    //public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    //{
+    //    public ApplicationDbContext()
+    //        : base("DefaultConnection", throwIfV1Schema: false)
+    //    {
+    //    }
+
+    //    public static ApplicationDbContext Create()
+    //    {
+    //        return new ApplicationDbContext();
+    //    }
+    //}
+
+    public class ApplicationDbContext
     {
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+        public static DX.Data.Xpo.XpoDatabase Create()
+        {
+            return new DX.Data.Xpo.XpoDatabase("DefaultConnection");
+        }
+    }
+
+    //new code for xpo identity
+
+    [MapInheritance(MapInheritanceType.ParentTable)]
+    public class XpoApplicationUser : XpoDxUser
+    {
+        public XpoApplicationUser(Session session) : base(session)
         {
         }
-
-        public static ApplicationDbContext Create()
+        public override void Assign(object source, int loadingFlags)
         {
-            return new ApplicationDbContext();
+            base.Assign(source, loadingFlags);
+            //ApplicationUser src = source as ApplicationUser;
+            //if (src != null)
+            //{
+            //	// additional properties here
+            //	this.PropertyA = src.PropertyA;
+            //	// etc.				
+            //}
+        }
+    }
+
+    public class ApplicationRole : XPIdentityRole<XpoApplicationRole>
+    {
+        public ApplicationRole(XpoApplicationRole source, int loadingFlags) : base(source, loadingFlags)
+        { }
+
+        public ApplicationRole(XpoApplicationRole source) : base(source)
+        { }
+
+        public ApplicationRole()
+        { }
+        public override void Assign(object source, int loadingFlags)
+        {
+            base.Assign(source, loadingFlags);
+            //XpoApplicationRole src = source as XpoApplicationRole;
+            //if (src != null)
+            //{
+            //  // additional properties here
+            //  this.PropertyA = src.PropertyA;
+            //  // etc.             
+            //}
+        }
+    }
+
+
+    [MapInheritance(MapInheritanceType.ParentTable)]
+    public class XpoApplicationRole : XpoDxRole
+    {
+        public XpoApplicationRole(Session session) : base(session)
+        {
+        }
+        public override void Assign(object source, int loadingFlags)
+        {
+            base.Assign(source, loadingFlags);
+            //ApplicationUser src = source as ApplicationUser;
+            //if (src != null)
+            //{
+            //  // additional properties here
+            //  this.PropertyA = src.PropertyA;
+            //  // etc.             
+            //}
         }
     }
 }
